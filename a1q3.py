dc64ce381c083497188a4b2f2d9043cb463ed526
Harry6542 <ozc189@usak.ca>
2023-05-30 21:00:20 -0600

    Written docstring for neighbours_around_cell function

--- a1q3.py
+++ a1q3.py
@@ -12,3 +12,12 @@ def Initial_state_reading(filename):
     begining_state = [list(line.strip()) for line in lines]
     return begining_state
 def neighbours_around_cell(displayed_grid, i, j):
+    """ Updates the status of a grid display based on the Game of Life rules.
+            Args: displayed_grid (list): A list of lists representing the current state of the grid display.
+                                         Each inner list represents a grid display row.
+                                         The characters in each row are saved as distinct inner list components.
+                                         The grid display can include the characters '*' to denote live cells
+                                         and '-' to represent dead cells.
+
+            Returns: list: The updated status of the grid display after applying the Game of Life rules.
+        """
